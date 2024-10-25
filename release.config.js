13305a3e3ec8c36798f757df0a6ac9ff6bfd5175
Ron Appleton <ron.appleton@gmail.com>
2024-10-25 22:38:50 +0100

    Specify dependancies

--- release.config.js
+++ release.config.js
@@ -14,7 +14,16 @@ module.exports = {
             },
         ],
     ],
-    "engines" : {
-        "node" : ">= 16.0.0"
+    "devDependencies": {
+        "semantic-release": "^19.0.0",
+        "@semantic-release/changelog": "^6.0.0",
+        "@semantic-release/git": "^10.0.0",
+        "@semantic-release/github": "^8.0.0",
+        "@semantic-release/commit-analyzer": "^9.0.0",
+        "@semantic-release/release-notes-generator": "^10.0.0",
+        "@semantic-release/npm": "^8.0.0"
+    },
+    "engines": {
+        "node": ">=16.0.0"
     }
 };
